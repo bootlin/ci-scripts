a75e25bcaf8eeb174f8dd2b0e87efbfb8db5c464
Florent <florent.jacquet@free-electrons.com>
2017-06-28 14:41:19 +0200

    ci-cron.sh: use new path
    Signed-off-by: Florent <florent.jacquet@free-electrons.com>

--- ci-cron.sh
+++ ci-cron.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-#
-# Skia < skia AT libskia DOT so >
-#
-# Beerware licensed software - 2017
-#
 
-ssh ci@build.free-electrons.com './launch_builds.sh'
+ssh ci@build.free-electrons.com './ci-scripts/launch_builds.sh'
 
 rsync -ravzP ci@build.free-electrons.com:storage/builds /srv/downloads/
 rsync -ravzP ci@build.free-electrons.com:storage/rootfs /srv/downloads/
