f0e34e1e105188530a9d7d6824c24fd3ebff3d7a
Florent Jacquet <florent.jacquet@free-electrons.com>
2017-06-28 16:49:51 +0200

    buildrootfs.sh: build if some artifacts are missing
    Signed-off-by: Florent Jacquet <florent.jacquet@free-electrons.com>

--- buildrootfs.sh
+++ buildrootfs.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 #
 
+STORAGE=$HOME/storage/rootfs
+BUILD=0
+
+if [ $(ls -1 $STORAGE |wc -l) -lt 8 ]; then
+    BUILD=1
+fi
 
 cd $HOME/buildroot-ci
-
 git fetch
 if ! [ "$(git rev-parse HEAD)" == "$(git rev-parse @{u})" ]; then
+    BUILD=1
+fi
+
+if [ $BUILD -eq 1 ]; then
     git pull
 
+    echo "Building rootfs"
     if ./build_rootfs.sh defconfig armv4 build armv4 defconfig armv5 build armv5 defconfig armv7 build armv7 defconfig aarch64 build aarch64
     then
         echo "Rootfs built!"
+        exit 0
     else
         echo "Rootfs building failed"
+        exit 1
     fi
-    exit 0
 else
     echo "Nothing to update"
     exit 0
