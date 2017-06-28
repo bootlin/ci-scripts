807317aefa5a3b3a1f91406317a2052a086c3969
Florent <florent.jacquet@free-electrons.com>
2017-06-28 14:48:18 +0200

    launch_builds.sh: adding some output
    Signed-off-by: Florent <florent.jacquet@free-electrons.com>

--- launch_builds.sh
+++ launch_builds.sh
@@ -2,11 +2,15 @@
 
 cd $(dirname $0)
 
+echo "Triggering rootfs build"
 ./buildrootfs.sh
 
+echo "Triggering kernels build"
 cd $HOME/kernel-builder/
 git pull
+echo "  Updating sources"
 ./update-source-tarball.sh all
+echo "  Launching builds"
 for d in $(ls defconfigs/*/*); do 
     ./build.py -d $d; 
 done
