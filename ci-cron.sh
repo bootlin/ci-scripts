248cef356d2ff5e468429a5bed3f74b24876f97b
Florent <florent.jacquet@free-electrons.com>
2017-06-28 14:44:12 +0200

    ci-cron.sh: adding some output
    Signed-off-by: Florent <florent.jacquet@free-electrons.com>

--- ci-cron.sh
+++ ci-cron.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+echo "Triggering builds on Build server"
 ssh ci@build.free-electrons.com './ci-scripts/launch_builds.sh'
 
+echo "Fetching storage from Build server"
 rsync -ravzP ci@build.free-electrons.com:storage/builds /srv/downloads/
 rsync -ravzP ci@build.free-electrons.com:storage/rootfs /srv/downloads/
 
+echo "Launching ctt to send custom jobs"
 cd $HOME/custom_tests_tool
-
+echo "  Updating ctt"
 git pull
-
 source env/bin/activate
-
 pip install --upgrade -r requirements.txt
+echo "  ctt up to date"
 
-./ctt.py -b all
+echo "  launching all jobs for mainline"
+./ctt.py -b all --tree mainline
+echo "  launching all jobs for next"
 ./ctt.py -b all --tree next
 
 
