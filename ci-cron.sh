cb79b7b1b2ab9fb6ccf5ac86a1f8fb3fd4604e7e
Florent <florent.jacquet@free-electrons.com>
2017-07-20 09:50:07 +0200

    ci-cron: specifying trees is not needed anymore in ctt
    Signed-off-by: Florent <florent.jacquet@free-electrons.com>

--- ci-cron.sh
+++ ci-cron.sh
@@ -51,8 +51,7 @@ fi
 echo "  ctt up to date"
 
 echo "  launching jobs"
-if  ! ./ctt.py -b all --tree mainline 2>&1 > $MAIN/ctt.log || \
-    ! ./ctt.py -b all --tree next 2>&1 >> $MAIN/ctt.log; then
+if  ! ./ctt.py -b all 2>&1 > $MAIN/ctt.log; then
     echo "There was an error launching the jobs"
     cat - > /tmp/ci_email <<EOF
 Subject: Launching daily jobs failed in CI architecture
