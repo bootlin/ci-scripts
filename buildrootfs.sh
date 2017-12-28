65b18faf7c6c3e47a3080b41bd1c81051fad5039
Mylène Josserand <mylene.josserand@free-electrons.com>
2017-12-28 17:50:59 +0100

    buildrootfs: Improve when rebuild images
    Currently, it is looking only between the HEAD and the
    upstream hashes to know if we need to rebuild rootfs.
    Improve it by adding a file with the last commit built
    so images will be rebuilt if there is a difference with
    the last build.
    
    Signed-off-by: Mylène Josserand <mylene.josserand@free-electrons.com>

--- buildrootfs.sh
+++ buildrootfs.sh
@@ -3,6 +3,7 @@
 
 STORAGE=$HOME/storage/rootfs
 BUILD=0
+LAST_BUILD=$HOME/last_build.txt
 
 if [ $(ls -1 $STORAGE |wc -l) -lt 8 ]; then
     BUILD=1
@@ -10,7 +11,7 @@ fi
 
 cd $HOME/buildroot-ci
 git fetch
-if ! [ "$(git rev-parse HEAD)" == "$(git rev-parse @{u})" ]; then
+if ! [ "`cat $LAST_BUILD`" == "$(git rev-parse @{u})" ]; then
     BUILD=1
 fi
 
@@ -21,6 +22,7 @@ if [ $BUILD -eq 1 ]; then
     if ./build_rootfs.sh defconfig armv4 build armv4 defconfig armv5 build armv5 defconfig armv7 build armv7 defconfig aarch64 build aarch64 2>&1
     then
         echo "Rootfs built!"
+        echo $(git rev-parse HEAD) > $LAST_BUILD
         exit 0
     else
         echo "Rootfs building failed"
