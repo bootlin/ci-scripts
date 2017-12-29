18b9b830070a8ca39b22e091297aa11a8788856e
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-12-29 11:34:59 +0100

    ci-cron.sh: entirely remove logging and e-mail notification
    cron is already capable of logging the standard/error output, and
    sending them by e-mail. This patch should be committed after changing
    the crontab as follows:
    
    MAILTO=kernelci@free-electrons.com
    
    And we will get every day the complete logs by e-mail. Once the system
    is working fine and stable, we may decide to redirect the output of
    some commands to log files, but for now, having the complete output of
    each command sent by e-mail every day would be very useful.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- ci-cron.sh
+++ ci-cron.sh
@@ -1,67 +1,30 @@
 #!/bin/bash
 
-MAIL_TO="kernelci@free-electrons.com"
 cd $(dirname $0)
 MAIN=$(pwd)
 
 echo "Triggering builds on Build server"
 if ! ssh ci@build.free-electrons.com './ci-scripts/launch_builds.sh'; then
     echo "There was some errors during the build. Sending an email, but going further"
-    cat - > /tmp/ci_email <<EOF
-Subject: Build error(s) in CI architecture
-
-There was some errors in the daily build of rootfs and/or kernels.
-You can find the logs on the build server in the /home/ci/ci-scripts folder.
-
-EOF
-    /usr/sbin/sendmail $MAIL_TO < /tmp/ci_email
 fi
 
 echo "Fetching storage from Build server"
-if  ! rsync -ravzP ci@build.free-electrons.com:storage/builds /srv/downloads/ 2>&1 > $MAIN/rsync.log || \
-    ! rsync -ravzP ci@build.free-electrons.com:storage/rootfs /srv/downloads/ 2>&1 >> $MAIN/rsync.log; then
+if  ! rsync -ravzP ci@build.free-electrons.com:storage/builds /srv/downloads/ || \
+    ! rsync -ravzP ci@build.free-electrons.com:storage/rootfs /srv/downloads/ ; then
     echo "There was a problem getting the artifacts from the build server"
-    cat - > /tmp/ci_email <<EOF
-Subject: Transfer error(s) in CI architecture
-
-There was some errors to fetch the latest builds.
-You can find the logs on the LAVA VM in the /home/ctt/ci-scripts folder.
-
-EOF
-    /usr/sbin/sendmail $MAIL_TO < /tmp/ci_email
 fi
 
 echo "Launching ctt to send custom jobs"
 cd $HOME/custom_tests_tool
 echo "  Updating ctt"
 source env/bin/activate
-if  ! git pull 2>&1 > $MAIN/git_pull.log || \
-    ! pip install --upgrade -r requirements.txt 2>&1 > $MAIN/pip.log; then
+if  ! git pull || \
+    ! pip install --upgrade -r requirements.txt ; then
     echo "There was an error getting the latest CTT version"
-    cat - > /tmp/ci_email <<EOF
-Subject: Getting the latest CTT version failed
-
-There was some errors updating CTT in the LAVA VM, either with the \`git pull\`,
-or the dependencies installation.
-You can find the logs on the LAVA VM in the /home/ctt/ci-scripts folder.
-
-EOF
-    /usr/sbin/sendmail $MAIL_TO < /tmp/ci_email
 fi
 echo "  ctt up to date"
 
 echo "  launching jobs"
-if  ! ./ci_launcher.py -b all 2>&1 > $MAIN/ctt.log; then
+if  ! ./ci_launcher.py -b all; then
     echo "There was an error launching the jobs"
-    cat - > /tmp/ci_email <<EOF
-Subject: Launching daily jobs failed in CI architecture
-
-There was some errors sending the jobs.
-You can find the logs on the LAVA VM in the /home/ctt/ci-scripts folder.
-
-EOF
-    /usr/sbin/sendmail $MAIL_TO < /tmp/ci_email
 fi
-
-
-
